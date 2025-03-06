745d8fc8d060f568cc8663af4c8dc5c7134b7fa6
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2025-03-06 01:33:32 +0100

    Updated the sqlalchemy section

--- recursos/leccion7/sqlalchemy/productos/main.py
+++ recursos/leccion7/sqlalchemy/productos/main.py
@@ -1,4 +1,7 @@
-from db import *
+"""Módulo principal del programa"""
+
+from sqlalchemy import insert, select, update, delete, exc
+from db import Base, engine, session
 from models import Productos
 
 
@@ -36,7 +39,7 @@ def consultar_data():
 def consultar_id_data(producto_id):
     print("\n¡Consulta de producto en base a su clave primaria!")
     # SELECT * FROM productos WHERE id == 1
-    producto = session.query(Productos).get(producto_id)
+    producto = session.get(Productos, producto_id)
     print(producto)
 
 
--- source/leccion7/index.rst
+++ source/leccion7/index.rst
@@ -779,7 +779,8 @@ tabla usando ``SQLAlchemy``.
 .. raw:: html
    :file: ../_templates/partials/soporte_profesional.html
 
-.. disqus::
+..
+    .. disqus::
 
 .. _`SQLAlchemy`: https://pypi.org/project/SQLAlchemy/
 .. _`MongoDB`: https://www.mongodb.com/
